af27fa185675c0fcac3f62d1918124c1e3462283
John Williams <porschemad911@gmail.com>
2025-10-24 21:51:40 +1100

    S922X - linux 6.17.4

--- projects/ROCKNIX/packages/linux-drivers/mali-bifrost/package.mk
+++ projects/ROCKNIX/packages/linux-drivers/mali-bifrost/package.mk
@@ -11,6 +11,17 @@ PKG_LONGDESC="mali-bifrost: Linux drivers for Mali Bifrost GPUs"
 PKG_TOOLCHAIN="manual"
 PKG_IS_KERNEL_PKG="yes"
 
+if [ "${DEVICE}" = "S922X" ]; then
+  # For S922X use sydarn's ported driver, no patches required for build
+  PKG_VERSION="f86d3dd4923b5d5de11ae7eaf7a6c4fee136528e" # BX304L01B-SW-99002-r54p1-12eac0
+  PKG_SITE="https://github.com/sydarn/mali_kbase"
+  PKG_URL="${PKG_SITE}.git"
+  PKG_GIT_CLONE_BRANCH="bifrost_port"
+else
+  # Patches required for build
+  PKG_PATCH_DIRS+=" 6.12-LTS"
+fi
+
 make_target() {
   kernel_make KDIR=$(kernel_path) -C ${PKG_BUILD} \
        CONFIG_MALI_MIDGARD=m CONFIG_MALI_PLATFORM_NAME=meson CONFIG_MALI_REAL_HW=y CONFIG_MALI_DEVFREQ=y CONFIG_MALI_GATOR_SUPPORT=y
